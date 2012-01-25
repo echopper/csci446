33d21ff26fecb9b720896f617dad282cb809f8ac
Ezekiel Chopper <echopper@mines.edu>
2012-01-24 23:39:51 -0700

    finished level 7!

--- project02/rubywarrior/ezekiel-beginner/player.rb
+++ project02/rubywarrior/ezekiel-beginner/player.rb
@@ -1,6 +1,14 @@
 class Player
   def play_turn(warrior)
-    if warrior.feel.empty?
+    if warrior.health < 10 
+      if warrior.feel(:backward).wall?
+        warrior.rest!
+      elsif warrior.feel(:backward).captive?
+        warrior.rescue!(:backward)
+      elsif warrior.feel(:backward).empty?
+        warrior.walk!(:backward)
+      end
+    elsif warrior.feel.empty?
       if warrior.health < 20
         if warrior.health < @health
           warrior.walk!
@@ -12,9 +20,9 @@ class Player
       end
     elsif warrior.feel.captive?
       warrior.rescue!
-    elsif
+    else
       warrior.attack!
-    end 
+    end
     @health = warrior.health
   end
 end
