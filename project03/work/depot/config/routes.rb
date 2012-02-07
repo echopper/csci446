acd1d2a3a418eccb451edd7377176db4ebe1d736
Ezekiel Chopper <echopper@mines.edu>
2012-02-07 08:39:30 -0700

    found the error in line_items.yml which was causing the SQL queries to go wrong and breaking all the tests. No fix yet for the "missing" will_paginate

--- project03/work/depot/config/routes.rb
+++ project03/work/depot/config/routes.rb
@@ -7,10 +7,11 @@ Depot::Application.routes.draw do
 
   get "store/index"
 
-  resources :products
+  resources :products do
     get :who_bought, on: :member
   end
 
+  # ...
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -62,6 +63,8 @@ Depot::Application.routes.draw do
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
   root to: 'store#index', as: 'store'
+  # ...
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
