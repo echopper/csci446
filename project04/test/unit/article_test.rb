283c8236554b9eb6a3f1270e851a06afcb316ff9
Ezekiel Chopper <echopper@mines.edu>
2012-02-17 20:18:53 -0700

    migrated the database. And all tests pass(!) *happy dance*

--- project04/test/unit/article_test.rb
+++ project04/test/unit/article_test.rb
@@ -3,10 +3,10 @@ require 'test_helper'
 class ArticleTest < ActiveSupport::TestCase
   test "article should not allow emtpy creation" do
     article = Article.new
-    assert product.invalid?
-    assert product.errors[:title].any?
-    assert product.errors[:author_name].any?
-    assert product.errors[:body].any?
-    assert product.number_of_edits == 0
+    assert article.invalid?
+    assert article.errors[:title].any?
+    assert article.errors[:author_name].any?
+    assert article.errors[:body].any?
+    assert article.number_of_edits == 0
   end
 end
