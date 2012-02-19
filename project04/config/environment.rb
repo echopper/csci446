a540da4d3d240e3501ed7427bd7b2b3f2d059102
Ezekiel Chopper <echopper@mines.edu>
2012-02-19 01:17:42 -0700

    Customized the layout a bit, got the dats to format correctly and replaced show, edit, destory with images. destroy does not destroy anything though.

--- project04/config/environment.rb
+++ project04/config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Articles::Application.initialize!
+
+Time::DATE_FORMATS[:us_with_weekday] = "%A, %b. %e %Y"
+Time::DATE_FORMATS[:us_sans_weekday] = "%b. %e %Y"
