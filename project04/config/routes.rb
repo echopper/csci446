af41deae4cfda5c45bfb840b47453917718e6d89
Ezekiel Chopper <echopper@mines.edu>
2012-02-20 01:11:39 -0700

    Fixed a bug in routes where an "as" broke pretty much all articles functionality in one way or another.

--- project04/config/routes.rb
+++ project04/config/routes.rb
@@ -58,6 +58,6 @@ Articles::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 
-  root to: 'articles#index', as: 'articles'
+  root to: 'articles#index'
 
 end
