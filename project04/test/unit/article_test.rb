cac09b5916dbc10d8b6d425615b53fbb9ce9d82b
Ezekiel Chopper <echopper@mines.edu>
2012-02-20 11:53:19 -0700

    Wrote unit tests to ensure that articles were created correctly. Forgot to sift through authors and deny Pat

--- project04/test/unit/article_test.rb
+++ project04/test/unit/article_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
+  
   test "article should not allow emtpy creation" do
     article = Article.new
     assert article.invalid?
@@ -9,4 +10,22 @@ class ArticleTest < ActiveSupport::TestCase
     assert article.errors[:body].any?
     assert article.number_of_edits == 0
   end
+
+  test "no articles by Pat are allowed" do
+    article = Article.new(title: "Exposition on the Bengal Tiger",
+                          author_name: "Pat Beauregard",
+                          body: "The Bengal Tiger is an animal which is orange with black stripes.")
+    assert article.invalid?
+    assert article.errors[:title].none?
+    assert article.errors[:author_name].any?
+    assert article.errors[:body].none?
+  end
+
+  test "good article" do
+    article = Article.new(title: "Exposition on the Bengal Tiger",
+                          author_name: "Veruca Salt",
+                          body: "The Bengal Tiger is an animal which is orange with black stripes.")
+    assert article.valid?
+  end
+
 end
