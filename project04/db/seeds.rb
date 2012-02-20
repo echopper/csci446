5fc36edba5f34cb94b01fe9c9efc72b73a2b4e9f
Ezekiel Chopper <echopper@mines.edu>
2012-02-20 11:30:52 -0700

    Made Edit and New forms have different text on buttons. All pages are now functionally complete.

--- project04/db/seeds.rb
+++ project04/db/seeds.rb
@@ -12,17 +12,5 @@ Article.delete_all
   Article.create(title: 'Lorem Ipsum',
     author_name: 'Robert Frost',
     body: 
-      %{<p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque
-        felis ut augue aliquet et mattis risus rhoncus. Integer in augue neque, 
-        vitae accumsan velit. Ut eros turpis, ornare congue commodo id, condimentum 
-        gravida purus. Nullam scelerisque, velit sed sagittis pharetra, leo lectus 
-        sagittis quam, malesuada tristique neque odio in est. Donec ut arcu lacinia 
-        leo sagittis sagittis eget id purus. Donec pretium viverra urna ac volutpat. 
-        Nam vel lacus est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-        Suspendisse vulputate, libero eget fringilla elementum, purus nibh facilisis 
-        arcu, vitae sagittis risus quam vitae ante. Aenean ornare eros at eros consequat 
-        non varius diam sagittis. Cras laoreet leo eget nisi semper vitae malesuada 
-        tellus vestibulum. Quisque ut pulvinar velit.
-      </p>}) 
+      %{<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque felis ut augue aliquet et mattis risus rhoncus. Integer in augue neque, vitae accumsan velit. Ut eros turpis, ornare congue commodo id, condimentum gravida purus. Nullam scelerisque, velit sed sagittis pharetra, leo lectus sagittis quam, malesuada tristique neque odio in est. Donec ut arcu lacinia leo sagittis sagittis eget id purus. Donec pretium viverra urna ac volutpat. Nam vel lacus est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vulputate, libero eget fringilla elementum, purus nibh facilisis arcu, vitae sagittis risus quam vitae ante. Aenean ornare eros at eros consequat non varius diam sagittis. Cras laoreet leo eget nisi semper vitae malesuada tellus vestibulum. Quisque ut pulvinar velit.</p>}) 
 end
