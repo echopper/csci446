5a25ba2f3ebaec5bfdd86150d338e6945b8b66d8
Ezekiel Chopper <echopper@mines.edu>
2012-03-05 23:44:04 -0700

    wrote tests to ensure that authors are created correctly

--- project05/test/unit/author_test.rb
+++ project05/test/unit/author_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class AuthorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "author should not allow empty creation" do
+    author = Author.new
+    assert author.invalid?
+    assert author.errors[:name].any?
+  end
+
+  test "no authors by the name Pat are allowed" do
+    author = Author.new(name: "Pat Beauregard")
+    assert author.invalid?
+    assert author.errors[:name].any?
+  end
+
+  test "good author" do
+    author = Author.new(name: "civilian regs")
+    assert author.valid?
+    assert author.errors[:name].none?
+  end
+
 end
