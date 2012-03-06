972f1739ee14b7489f1f64b72ef0ce2869a72f44
Ezekiel Chopper <echopper@mines.edu>
2012-03-05 21:18:23 -0700

    migrated the database, and created a migration to allow for author's o have pictures.

--- project05/db/schema.rb
+++ project05/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120218014403) do
+ActiveRecord::Schema.define(:version => 20120305172932) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(:version => 20120218014403) do
     t.integer  "number_of_edits", :default => 0
     t.datetime "created_at",                     :null => false
     t.datetime "updated_at",                     :null => false
+  end
+
+  create_table "authors", :force => true do |t|
+    t.string   "name",       :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
