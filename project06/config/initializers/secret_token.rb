855068e6daf75e23aef107c521f068ca3849064b
Ezekiel Chopper <echopper@mines.edu>
2012-04-11 11:20:11 -0600

    Created Project 6, updated the Gemfile.

---
+++ project06/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamez::Application.config.secret_token = '493f7f0c88792501016da410d0757da6a9d146de1e3965ac638a4859e33148368af08dc9c82b22fa9689ab8c136405cc9922bc58fb16216dddb3aea06d2d680f'
