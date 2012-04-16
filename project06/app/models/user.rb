17668ef0980c81c102297f983128e4509541fe93
Ezekiel Chopper <echopper@mines.edu>
2012-04-15 22:06:46 -0600

    tried to add logging filter but it threw an error, but something is already filtering, no big.

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
   acts_as_authentic
-  attr_accessible :email, :first_name, :games_count, :last_name, :password, :username
+  attr_accessible :email, :first_name, :last_name, :username, :password, :password_confirmation
 end
