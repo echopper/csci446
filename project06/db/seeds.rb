c62b19297e6f5699618a62bc437f7b3148f3363b
Ezekiel Chopper <echopper@mines.edu>
2012-04-16 23:46:11 -0600

    Started Declarative authorization and added users seeds

--- project06/db/seeds.rb
+++ project06/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username:              'gaiminator',
+            password:              'password',
+            password_confirmation: 'password',
+            email:                 'a@email.com',
+            first_name:            'Howie',
+            last_name:             'Gaimur'
+           )
+
+User.create(username:              'admin',
+            password:              'password',
+            password_confirmation: 'password',
+            email:                 'creator@gamez.com',
+            first_name:            'Philip',
+            last_name:             'Greenspun'
+           )
