7dc9afd2342176e20a709dafb0cb07a097f22abf
Ezekiel Chopper <echopper@mines.edu>
2012-04-16 22:03:59 -0600

    Got Authlogic working beautifully

--- project06/config/routes.rb
+++ project06/config/routes.rb
@@ -1,7 +1,9 @@
 Gamez::Application.routes.draw do
   resources :users
+  resources :user_sessions
 
-#  match '/login' => 'user_sessions#new', as: 'login'
+  match '/login' => 'user_sessions#new', as: 'login'
+  match '/logout' => 'user_sessions#destroy', as: 'logout'
 
   root to: 'users#index'
 
